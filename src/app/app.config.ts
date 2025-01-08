5628391faab051c642b1b221a20dd9a80cb7cedf
Harshada Bhondave <manishabhondave640@gmail.com>
2025-01-08 12:14:50 +0530

    revert by 3 commits

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient() ,provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideHttpClient() ,provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
